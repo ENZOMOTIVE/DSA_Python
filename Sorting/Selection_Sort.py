031cc33a4d8cec741b9c92db733f332484b12661
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2026-01-28 14:54:27 +0530

    All Sort

--- Sorting/Selection_Sort.py
+++ Sorting/Selection_Sort.py
@@ -16,14 +16,15 @@ def Selection_Sort(the_list):
   
 ## Improved Selection Sort with Swaping technique
 def Swap_Selection_Sort(the_list):
-
   n = len(the_list)
   for i in range(n-1):
     min_index = i
-    for j in range(i+1, n):
+    for j in range(i+1,n):
       if the_list[j] < the_list[min_index]:
-        the_list[i], the_list[min_index] = the_list[min_index], the_list[i] 
-    return the_list
+        min_index = j
+        
+    the_list[i], the_list[min_index] = the_list[min_index], the_list[i]
+  return the_list
 
 
 print("This is the normal algo:")
